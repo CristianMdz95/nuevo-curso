e9744197dc258c685029522951abd401e96df4bc
CristianMdz95 <officialcmendoza@gmail.com>
2022-01-05 17:50:12 -0600

    nuevos cambios

--- src/App.js
+++ src/App.js
@@ -1,32 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export function App() {
 
+  const [linea1, setLinea1] = useState('');
+  const [linea2, setLinea2] = useState('');
+  const [imagen, setImagen] = useState('');
+
+  const onChangeLinea1 = function(evento){
+      setLinea1( evento.target.value);
+  }
+
+  const onChangeLinea2 = function(evento){
+      setLinea2(evento.target.value);
+  }
+
+  const onChangeImagen = function(evento){
+    let imagen = '/img/'+ evento.target.value
+    setImagen(imagen);
+}
+
   return (
-      <div>
+      <div className='App'>
         <center>
-        <select>
-          <option value="prueba1">Prueba 1</option>
-          <option value="prueba2">Prueba 2</option>
-          <option value="prueba3">Prueba 3</option>
-          <option value="prueba4">Prueba 4</option>
+        <select onChange={onChangeImagen}>
+          <option value="Captura.png">Prueba 1</option>
+          <option value="Captura2.png">Prueba 2</option>
+          <option value="Captura3.png">Prueba 3</option>
+          <option value="Captura4.png">Prueba 4</option>
         </select>
         <br/><br/>
 
-        <input type="text" placeholder='Linea 1'></input>
+        <input onChange={onChangeLinea1} type="text" placeholder='Linea 1'></input>
         <br/>
-        <input type="text" placeholder='Linea 2'></input>
+        <input onChange={onChangeLinea2} type="text" placeholder='Linea 2'></input>
+        
         <br/><br/>
         <button>Exportar</button>
-
+        <br/>
         <div>
-          <span>Linea 1</span>
-          <span>Linea 2</span>
-          <img src=''/>
+          <span>{linea1}</span> <br/>
+          <span>{linea2}</span> <br/> 
+          <img src={imagen}/>
         </div>
 
 
-        </center>
+        </center> 
       </div>
   )
 }
